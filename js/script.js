b2fd6183a641353cd5f25256e2ae5a4f26ce5bfb
maks0n4ik <64151930+maks0n4ik@users.noreply.github.com>
2020-06-21 14:44:36 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let contactLink = document.querySelector('.contact')
+let underFooterLink = document.querySelector('.under-footer-link')
+
+contactLink.addEventListener('click', (e) => {
+  e.preventDefault()
+})
+
+underFooterLink.addEventListener('click', (e) => {
+  e.preventDefault()
+})
